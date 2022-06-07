5a53d7bce904893ec55c8d00faa29fce48bc8209
captain-mike <info@grafikweb.it>
2022-06-07 12:26:52 +0200

    reactive forms

--- m3/w3/d2/form/src/app/app-routing.module.ts
+++ m3/w3/d2/form/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Form1Component } from './form1/form1.component';
 import { Form2Component } from './form2/form2.component';
+import { Form3Component } from './form3/form3.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:'form2',
     component: Form2Component
+  },
+  {
+    path:'form3',
+    component: Form3Component
   }
 ];
 
--- m3/w3/d2/form/src/app/app.module.ts
+++ m3/w3/d2/form/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Form1Component } from './form1/form1.component';
 import { Form2Component } from './form2/form2.component';
+import { Form3Component } from './form3/form3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Form1Component,
-    Form2Component
+    Form2Component,
+    Form3Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
