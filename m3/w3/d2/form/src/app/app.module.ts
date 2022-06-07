0ae578b1e1e46afd2c95a37e7a49df49e854c168
captain-mike <info@grafikweb.it>
2022-06-07 11:10:21 +0200

    forms

---
+++ m3/w3/d2/form/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Form1Component } from './form1/form1.component';
+import { Form2Component } from './form2/form2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Form1Component,
+    Form2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
